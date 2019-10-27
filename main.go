71df3af71a911c6d168cf56746130f1d75407d4c
box <m.mcisse@icloud.com>
2019-10-27 23:17:10 +0100

    main.go file

--- main.go
+++ main.go
@@ -1,9 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+	"strconv"
+)
+
+//Will be using these port for port scanning later
+/*
+var ports = []string{
+	"80",
+	"21",
+	"22",
+	"443",
+	"8080",
+}
+*/
 
 func main() {
-	fmt.Println("vim-go")
+	ActiveThread := 0
+	doneChann := make(chan bool)
+	subNet := "192.168.100"
 
-	fmt.Println("Nothing to do yet")
+	for i := 0; i < 256; i++ {
+
+		realSubnet := subNet + "." + strconv.Itoa(i)
+		go resolvName(realSubnet, doneChann)
+		ActiveThread++
+	}
+
+	for ActiveThread > 0 {
+		<-doneChann
+		ActiveThread--
+	}
+}
+func resolvName(addr string, doneChann chan bool) {
+
+	hostname, err := net.LookupAddr(addr)
+	if err == nil {
+		log.Printf("%v -- %v", addr, hostname)
+	}
+
+	doneChann <- true
 }
